920a3c25c8252b1749e7127009b3791af9923c1c
X-jaekid-X <dubdubdot@proton.me>
2025-09-23 22:43:21 -0700

    T H E  M A T R I X

--- matrix.py
+++ matrix.py
@@ -10,69 +10,184 @@ import curses
 import random
 import time
 
-# US ASCII characters
-MATRIX_CHARS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*")
+MATRIX_CHARS = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%&*+><=|/")
+
+# ASCII art letters for big font
+BIG_FONT = {
+    "A": ["  A  ",
+          " A A ",
+          "AAAAA",
+          "A   A",
+          "A   A"],
+    "B": ["BBBB ",
+          "B   B",
+          "BBBB ",
+          "B   B",
+          "BBBB "],
+    "C": [" CCC ",
+          "C   C",
+          "C    ",
+          "C   C",
+          " CCC "],
+    "D": ["DDDD ",
+          "D   D",
+          "D   D",
+          "D   D",
+          "DDDD "],
+    "E": ["EEEEE",
+          "E    ",
+          "EEE  ",
+          "E    ",
+          "EEEEE"],
+    "H": ["H   H",
+          "H   H",
+          "HHHHH",
+          "H   H",
+          "H   H"],
+    "I": ["IIIII",
+          "  I  ",
+          "  I  ",
+          "  I  ",
+          "IIIII"],
+    "K": ["K   K",
+          "K  K ",
+          "KKK  ",
+          "K  K ",
+          "K   K"],
+    "M": ["M   M",
+          "MM MM",
+          "M M M",
+          "M   M",
+          "M   M"],
+    "R": ["RRRR ",
+          "R   R",
+          "RRRR ",
+          "R R  ",
+          "R  RR"],
+    "T": ["TTTTT",
+          "  T  ",
+          "  T  ",
+          "  T  ",
+          "  T  "],
+    "X": ["X   X",
+          " X X ",
+          "  X  ",
+          " X X ",
+          "X   X"],
+    "Y": ["Y   Y",
+          " Y Y ",
+          "  Y  ",
+          "  Y  ",
+          "  Y  "],
+    " ": ["     ",
+          "     ",
+          "     ",
+          "     ",
+          "     "],
+    "J": ["  JJJ",
+          "   J ",
+          "   J ",
+          "J  J ",
+          " JJ  "],
+    "D": ["DDDD ",
+          "D   D",
+          "D   D",
+          "D   D",
+          "DDDD "],
+}
+
+def big_text_lines(word):
+    """Convert a word into list of strings representing big ASCII art lines"""
+    lines = [""] * 5
+    for c in word.upper():
+        char_art = BIG_FONT.get(c, BIG_FONT[" "])
+        for i in range(5):
+            lines[i] += char_art[i] + "  "  # 2 spaces between letters
+    return lines
+
+def display_banner(stdscr, duration=3):
+    curses.curs_set(0)
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_GREEN, -1)
+    
+    stdscr.nodelay(True)
+    height, width = stdscr.getmaxyx()
+    words = ["THE", "MATRIX", "by JAEKID"]
+    
+    total_lines = len(words) * 5 + (len(words) - 1)
+    start_y = max((height - total_lines)//2, 0)
+    
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        stdscr.erase()
+        for w_idx, word in enumerate(words):
+            lines = big_text_lines(word)
+            for l_idx, line in enumerate(lines):
+                y = start_y + w_idx*6 + l_idx
+                x = max((width - len(line))//2, 0)
+                display_line = ""
+                for ch in line:
+                    if ch != " " and random.random() < 0.4:  # 40% chance to replace
+                        display_line += random.choice(MATRIX_CHARS)
+                    else:
+                        display_line += ch
+                try:
+                    stdscr.addstr(y, x, display_line, curses.color_pair(1) | curses.A_BOLD)
+                except curses.error:
+                    pass
+        stdscr.refresh()
+        time.sleep(0.1)
+        key = stdscr.getch()
+        if key != -1:
+            break
 
 def matrix_rain(stdscr):
     curses.curs_set(0)
     curses.start_color()
     curses.use_default_colors()
-
-    # Colors: head (white), bright green, dim green
-    curses.init_pair(1, curses.COLOR_WHITE, -1)  # head
-    curses.init_pair(2, curses.COLOR_GREEN, -1)  # bright trail
-    curses.init_pair(3, curses.COLOR_GREEN, -1)  # dim trail
+    curses.init_pair(1, curses.COLOR_GREEN, -1)
 
     height, width = stdscr.getmaxyx()
     columns = [[] for _ in range(width)]
-
     stdscr.nodelay(True)
-
-    density = 0.2       # Full screen
-    speed = 0.05        # Falling speed
-    trail_length = 6    # Length of fading trail
+    density = 0.2
+    speed = 0.05
+    trail_length = 6
 
     while True:
+        stdscr.erase()
+        for x in range(width):
+            if random.random() < density:
+                columns[x].append(0)
+            new_positions = []
+            max_y = max(columns[x]) if columns[x] else 0
+            for y in columns[x]:
+                if 0 <= y < height:
+                    char = random.choice(MATRIX_CHARS)
+                    try:
+                        stdscr.addch(y, x, char, curses.color_pair(1))
+                    except curses.error:
+                        pass
+                    new_positions.append(y + 1)
+            columns[x] = [pos for pos in new_positions if pos < height + trail_length]
+        stdscr.refresh()
+        time.sleep(speed)
         key = stdscr.getch()
         if key != -1:
             break
 
-        stdscr.erase()
-
-        for x in range(width):
-            # Chance to start a new stream
-            if random.random() < density:
-                columns[x].append(0)
-
-            new_positions = []
-            for y in columns[x]:
-                if 0 <= y < height:
-                    char = random.choice(MATRIX_CHARS)
-                    dist_from_head = y - max(columns[x])
-
-                    if dist_from_head == 0 and y >= 3:
-                        color = curses.color_pair(1)  # White head
-                    elif 1 <= dist_from_head <= 2:
-                        color = curses.color_pair(2)  # Bright green
-                    elif 3 <= dist_from_head < trail_length:
-                        color = curses.color_pair(3)  # Dim green
-                    else:
-                        color = curses.color_pair(2)
-
-                    # Ensure we don't write past the right edge
-                    if 0 <= x < width:
-                        try:
-                            stdscr.addstr(y, x, char, color)
-                        except curses.error:
-                            pass
-
-                    new_positions.append(y + 1)
-
-            # Keep positions within screen height + trail
-            columns[x] = [pos for pos in new_positions if pos < height + trail_length]
-
-        stdscr.refresh()
-        time.sleep(speed)
+def main(stdscr):
+    display_banner(stdscr, duration=4)  # show dynamic banner for 4 seconds
+    matrix_rain(stdscr)
 
 if __name__ == "__main__":
-    curses.wrapper(matrix_rain)
+    curses.wrapper(main)
+
+
+
+
+
+
+
+
